e53dec05e4485907b63db35602209dc61a7e9576
Paulo Henrique <pauloh16.pdr@gmail.com>
2019-10-14 19:18:26 -0300

    Finalizar segunda aula

---
+++ Curso em video/2º modulo/script001.js
@@ -0,0 +1,2 @@
+let nome = window.prompt('Qual seu nome?');
+window.alert(`É um grande prazer te conhecer ${nome}!`);
---
+++ Curso em video/2º modulo/script002.js
@@ -0,0 +1,5 @@
+let n1 = window.Number(prompt("Digite um numero:"));
+let n2 = window.Number(prompt("Digite outro numero:"));
+let soma = n1 + n2;
+
+window.alert(`A soma entre ${n1} e ${n2} é: ${String(soma)}`);
