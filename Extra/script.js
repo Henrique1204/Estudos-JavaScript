8ee3fee8f5943ea352d5e1e322b23ae03d569fa7
Paulo <pauloh16.pdr@gmail.com>
2019-11-01 20:01:17 -0300

    Adicionar section 'Contar Passo'

--- Extra/script.js
+++ Extra/script.js
@@ -10,6 +10,11 @@ const imagemDoPeriodoDoDia = document.querySelector('#periodoDoDia img');
 const fAno = document.getElementById('txtano');
 const botaoVerificarIdade = document.getElementById('botaoVerificarIdade');
 const resultadoVerificarIdade = document.getElementById('mostrarIdade');
+const botaoContarPasso = document.querySelector('#contarPasso button');
+const inicio = document.getElementById('inicio');
+const fim = document.getElementById('fim');
+const passo = document.getElementById('passo');
+const resultadoContaPasso = document.querySelector('#contarPasso p');
 let comprimento;
 
 function verificarPeriodoDoDia(){
@@ -20,7 +25,7 @@ function verificarPeriodoDoDia(){
         return comprimento = 'Bom dia';
     }else if(hora < 18){
         return comprimento = 'Boa tarde';
-    }else if(hora < 1){
+    }else if(hora < 24){
         return comprimento = 'Boa noite';
     }else{
         return comprimento = 'Boa madrugada';
@@ -180,4 +185,51 @@ fAno.addEventListener('keyup', function(evento){
     if(evento.keyCode === 13){
         verificarIdade();
     };
+});
+
+botaoContarPasso.addEventListener('click', function contar(){
+
+
+    if(inicio.value != '' && fim.value != '' && passo.value != ''){
+
+        resultadoContaPasso.innerText = 'Contando:';
+
+        if(passo.value > 0){
+
+            if(inicio.value < fim.value){
+
+                for(i = Number(inicio.value); i <= Number(fim.value); i += Number(passo.value)){
+                    resultadoContaPasso.innerText += ` ${i} \u{1f449} `;
+                }; 
+                resultadoContaPasso.innerText += `\u{1F3F4}`;
+
+            }else{
+                for(i = Number(inicio.value); i >= Number(fim.value); i -= Number(passo.value)){
+                    resultadoContaPasso.innerText += ` ${i} \u{1f449} `;
+                }; 
+                resultadoContaPasso.innerText += `\u{1F3F4}`;
+            };
+
+        }else{
+
+            if(inicio.value < fim.value){
+                alert('[ERRO] passo não é válido. o passo será considerado igual a 1')
+                for(i = inicio.value; i <= fim.value; i++){
+                    resultadoContaPasso.innerText += ` ${i} \u{1F449} `
+                };
+                resultadoContaPasso.innerText += `\u{1F3F4}`;
+                
+            }else{
+                alert('[ERRO] passo não é válido. o passo será considerado igual a 1')
+                for(i = inicio.value; i >= fim.value; i--){
+                    resultadoContaPasso.innerText += ` ${i} \u{1F449} `
+                };
+                resultadoContaPasso.innerText += `\u{1F3F4}`; 
+            };
+        };
+
+    }else{
+        resultadoContaPasso.innerText = 'Impossível contar, preencha todos os campos'
+    };
+
 });
